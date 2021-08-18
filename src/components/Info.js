228dff72dc70d2f4df9f01702b3e1bd64c0c3d2c
OlgaRhythm <87225985+OlgaRhythm@users.noreply.github.com>
2021-08-18 13:46:28 +0300

    Add files via upload

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Приветствуем Вас в приложени</h1>
+            </div>
+        );
+    }
+}
+
+export default Info;
