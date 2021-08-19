f41436070fbe0b480ccdb3eaff421dda78dcf0ac
OlgaRhythm <87225985+OlgaRhythm@users.noreply.github.com>
2021-08-19 23:40:21 +0300

    Add files via upload

---
+++ src/components/Weather.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.error}</p>
+                {this.props.city &&
+                    <div>
+                        <p>Местоположение {this.props.city}, {this.props.country}</p>
+                        <p>Температура {this.props.temp}</p>
+                        <p>Давление {this.props.pressure}</p>
+                        <p>Восход солнца {this.props.sunrise}</p>
+                        <p>Заход солнца {this.props.sunset}</p>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default Weather;
